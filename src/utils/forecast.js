b8cb71c2eb2f7cd281774afe06f1194790a1d512
be-single-she-wolf <be-single-she-wolf@MacBook-Pro.local>
2020-06-22 09:39:17 +0300

    add more info in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             }else if(body.error){
                 callback('Unable to find location');
             } else{
-                callback(undefined,`${body.current.weather_descriptions[0]}.It's ${body.current.temperature} degress out. It's feels like ${body.current.feelslike} degress out.`);
+                callback(undefined,`${body.current.weather_descriptions[0]}.It's ${body.current.temperature} degress out. It's feels like ${body.current.feelslike} degress out. It's ${body.current.cloudcover}% cloudcover.`);
             }
         
         })
