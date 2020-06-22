e79655082086299754cf306d92109c86abbe4805
be-single-she-wolf <be-single-she-wolf@MacBook-Pro.local>
2020-06-22 09:25:17 +0300

    add about text

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.get('', (req,res)=> {
 })
 
 app.get('/about', (req,res)=> {
-    res.render('about',{title:"About me", name:"Tanya F"});
+    res.render('about',{title:"About", name:"Tanya F"});
 })
 
 app.get('/help', (req,res)=> {
